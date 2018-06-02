9150334f064fdac2cd9d3bb54ef38aba86488ca1
Luiz Negrini <gnrmalazagnr@hotmail.com>
2018-06-02 14:07:38 -0300

    Initial Commit

---
+++ src/main/java/controllers/HelloWorldController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Path("/hello")
+public class HelloWorldController {
+
+	@GET
+	@Produces("text/plain")
+	public Response doGet() {
+		return Response.ok("Bombando").build();
+	}
+}
